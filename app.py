52c55df4021788e5464b3e7bccbaaa4990a0bffa
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-22 14:50:22 -0600

    fixed bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -11,7 +11,6 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 class Guide(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
     content = db.Column(db.String(144), unique=False)
@@ -20,13 +19,31 @@ class Guide(db.Model):
         self.title = title
         self.content = content
 
+
 class GuideSchema(ma.Schema):
     class Meta:
         fields = ('title', 'content')
 
+
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
+
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
